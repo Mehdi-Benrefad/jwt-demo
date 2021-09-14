15a7cbf16c9518fbbfa8e70f085e9a595a932628
Mehdi-Benrefad <benrefadmehdi@gmail.com>
2021-09-14 15:46:15 +0100

    Jwt util implementation

--- src/main/java/com/example/demo/config/JwtConfig.java
+++ src/main/java/com/example/demo/config/JwtConfig.java
@@ -1,11 +1,15 @@
 package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.example.demo.service.CustomUserDetailService;
 
@@ -30,7 +34,23 @@ public class JwtConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		super.configure(http);
+		//super.configure(http);
+		http
+	        .csrf()
+	        .disable()
+			.cors()
+	        .disable()
+	        .authorizeRequests()
+	        .antMatchers("/api/generateToken").permitAll()//only allow this endpoint without authentication
+	        .anyRequest().authenticated()//for any other request, authentication should performed
+	        .and()
+	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//every request should be independent of other and server does not have to manage session
+
 	}
+	
+	@Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
 }
